66dab4c4780918eb7dc6009ff5159e275960b7c8
jamorris <jamorris@users.noreply.github.com>
2019-04-26 13:22:59 -0500

    add time

--- cron/DeleteOldFiles.php
+++ cron/DeleteOldFiles.php
@@ -5,12 +5,12 @@ $dir = "../lightning/";
 
 /*** cycle through all files in the directory ***/
 foreach (glob($dir."*") as $file) {
-    
+
     /*** echo file info ***/
     echo "name: ", $file;
     echo " *** age in seconds: ", time() - filemtime($file);
     echo "<br>";
-    
+
     /*** if file is 24 hours (86400 seconds) old then delete it ***/
     if(time() - filemtime($file) > 86400){
         unlink($file);
@@ -19,6 +19,6 @@ foreach (glob($dir."*") as $file) {
 }
 
 /*** completion info ***/
-echo "finished at ", date(DATE_RFC2822);
+echo "finished at ", date(DATE_RFC2822), " time: ", time();
 
 ?>
